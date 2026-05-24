mikf/gallery-dl

commit a842d682111bf3ae32a008b186657acd06e04080

[8chan] remove cache decorators

gallery_dl/extractor/8chan.py
@@ -10,7 +10,6 @@
 
 from .common import Extractor, Message
 from .. import text, dt
-from ..cache import memcache
 import itertools
 
 BASE_PATTERN = r"(?:https?://)?8chan\.(moe|se|cc)"
@@ -25,7 +24,6 @@ class _8chanExtractor(Extractor):
         self.root = "https://8chan." + match[1]
         Extractor.__init__(self, match)
 
-    @memcache()
     def cookies_tos_name(self):
         domain = "8chan." + self.groups[0]
         for cookie in self.cookies:
@@ -46,7 +44,6 @@ class _8chanExtractor(Extractor):
         self.log.error("Unable to determin TOS cookie name")
         return "TOS20250418"
 
-    @memcache()
     def cookies_prepare(self):
         # fetch captcha cookies
         # (necessary to download without getting interrupted)
@@ -81,7 +78,8 @@ class _8chanThreadExtractor(_8chanExtractor):
 
     def items(self):
         _, board, thread = self.groups
-        self.cookies.set(self.cookies_tos_name(), "1", domain=self.root[8:])
+        tos = self.cache(self.cookies_tos_name, _mem=0)
+        self.cookies.set(tos, "1", domain=self.root[8:])
 
         # fetch thread data
         url = f"{self.root}/{board}/res/{thread}."
@@ -91,7 +89,7 @@ class _8chanThreadExtractor(_8chanExtractor):
         thread["_http_headers"] = {"Referer": url + "html"}
 
         try:
-            self.cookies = self.cookies_prepare()
+            self.cookies = self.cache(self.cookies_prepare)
         except Exception as exc:
             self.log.debug("Failed to fetch captcha cookies:  %s: %s",
                            exc.__class__.__name__, exc, exc_info=exc)
@@ -120,7 +118,8 @@ class _8chanBoardExtractor(_8chanExtractor):
 
     def items(self):
         _, board, pnum = self.groups
-        self.cookies.set(self.cookies_tos_name(), "1", domain=self.root[8:])
+        tos = self.cache(self.cookies_tos_name)
+        self.cookies.set(tos, "1", domain=self.root[8:])
 
         pnum = text.parse_int(pnum, 1)
         url = f"{self.root}/{board}/{pnum}.json"

RecRivenVI/Gallery-Downloader

commit 57c5477b736735381278ac851deb60928842d1ff

[8chan] fix 'IndexError: tuple index out of range' (cb#113)
fixes regression introduced in a842d682111bf3ae32a008b186657acd06e04080

gallery_dl/extractor/8chan.py
@@ -78,7 +78,7 @@ class _8chanThreadExtractor(_8chanExtractor):
 
     def items(self):
         _, board, thread = self.groups
-        tos = self.cache(self.cookies_tos_name, _mem=0)
+        tos = self.cache(self.cookies_tos_name, _key=None)
         self.cookies.set(tos, "1", domain=self.root[8:])
 
         # fetch thread data
@@ -89,7 +89,7 @@ class _8chanThreadExtractor(_8chanExtractor):
         thread["_http_headers"] = {"Referer": url + "html"}
 
         try:
-            self.cookies = self.cache(self.cookies_prepare)
+            self.cookies = self.cache(self.cookies_prepare, _key=None)
         except Exception as exc:
             self.log.debug("Failed to fetch captcha cookies:  %s: %s",
                            exc.__class__.__name__, exc, exc_info=exc)
@@ -118,7 +118,7 @@ class _8chanBoardExtractor(_8chanExtractor):
 
     def items(self):
         _, board, pnum = self.groups
-        tos = self.cache(self.cookies_tos_name)
+        tos = self.cache(self.cookies_tos_name, _key=None)
         self.cookies.set(tos, "1", domain=self.root[8:])
 
         pnum = text.parse_int(pnum, 1)