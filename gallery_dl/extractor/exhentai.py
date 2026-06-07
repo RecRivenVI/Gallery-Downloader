featherbutt/gallery-dl

commit 8893dd14023a653a327496985682bde4fdec0954

[exhentai] always forward search query parameters (#79)

gallery_dl/extractor/exhentai.py
@@ -680,24 +680,9 @@ class ExhentaiGalleryExtractor(ExhentaiExtractor):
 class ExhentaiSearchExtractor(ExhentaiExtractor):
     """Extractor for exhentai search results"""
     subcategory = "search"
-    pattern = BASE_PATTERN + r"/(?:\?([^#]*)|tag/([^/?#]+))"
+    pattern = (BASE_PATTERN +
+               r"/(?!favorites\.php)(?:tag/([^/?#]+))?(?:\?([^#]*))?")
     example = "https://e-hentai.org/?f_search=QUERY"
-
-    def __init__(self, match):
-        ExhentaiExtractor.__init__(self, match)
-
-        _, query, tag = self.groups
-        if tag:
-            if "+" in tag:
-                ns, _, tag = tag.rpartition(":")
-                tag = f"{ns}:\"{tag.replace('+', ' ')}$\""
-            else:
-                tag += "$"
-            self.params = {"f_search": tag, "page": 0}
-        else:
-            self.params = text.parse_query(query)
-            if "next" not in self.params:
-                self.params["page"] = text.parse_int(self.params.get("page"))
 
     def _init(self):
         self.search_url = self.root
@@ -706,7 +691,18 @@ class ExhentaiSearchExtractor(ExhentaiExtractor):
         self.login()
         data = {"_extractor": ExhentaiGalleryExtractor}
         search_url = self.search_url
-        params = self.params
+
+        _, tag, query = self.groups
+        params = text.parse_query(query)
+        if "next" not in params:
+            params["page"] = text.parse_int(params.get("page"))
+        if tag is not None:
+            if "+" in tag:
+                ns, _, tag = tag.rpartition(":")
+                tag = f"{ns}:\"{tag.replace('+', ' ')}$\""
+            else:
+                tag += "$"
+            params["f_search"] = tag
 
         while True:
             last = None
@@ -737,7 +733,7 @@ class ExhentaiSearchExtractor(ExhentaiExtractor):
 class ExhentaiFavoriteExtractor(ExhentaiSearchExtractor):
     """Extractor for favorited exhentai galleries"""
     subcategory = "favorite"
-    pattern = BASE_PATTERN + r"/favorites\.php(?:\?([^#]*)())?"
+    pattern = BASE_PATTERN + r"/favorites\.php(?:\?()([^#]*))?"
     example = "https://e-hentai.org/favorites.php"
 
     def _init(self):

test/results/exhentai.py
@@ -139,6 +139,12 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://exhentai.org/tag/parody:touhou+project?next=3845466&seek=2025-05-05",
+    "#category": ("", "exhentai", "search"),
+    "#class"   : exhentai.ExhentaiSearchExtractor,
+},
+
+{
     "#url"     : "https://exhentai.org/?f_doujinshi=0&f_manga=0&f_artistcg=0&f_gamecg=0&f_western=0&f_non-h=1&f_imageset=0&f_cosplay=0&f_asianporn=0&f_misc=0&f_search=touhou&f_apply=Apply+Filter",
     "#category": ("", "exhentai", "search"),
     "#class"   : exhentai.ExhentaiSearchExtractor,

RecRivenVI/Gallery-Downloader

commit b0f6fc9b16dbc5453eba64a952624b5e0e3f8050

[exhentai:favorite] do not send empty 'f_search' parameters (#158)
fixes regression introduced in 8893dd14023a653a327496985682bde4fdec0954

gallery_dl/extractor/exhentai.py
@@ -705,7 +705,7 @@ class ExhentaiSearchExtractor(ExhentaiExtractor):
         params = text.parse_query(query)
         if "next" not in params:
             params["page"] = text.parse_int(params.get("page"))
-        if tag is not None:
+        if tag:
             if "+" in tag:
                 ns, _, tag = tag.rpartition(":")
                 tag = f"{ns}:\"{tag.replace('+', ' ')}$\""

gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.32.2"
+__version__ = "1.32.3-dev"
 __variant__ = None