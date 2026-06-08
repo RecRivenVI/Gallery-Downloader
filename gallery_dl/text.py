RecRivenVI/Gallery-Downloader

commit 0f63a5930ead1a6b6c5d8ea28c386f924ae5713f

[text] support 'int' values in 'build_query'

gallery_dl/text.py
@@ -378,7 +378,8 @@ def parse_query_list(qs, as_list=()):
 
 def build_query(params):
     return "&".join([
-        f"{quote(name)}={quote(value)}"
+        (f"{quote(name)}="
+         f"{quote(value if isinstance(value, str) else str(value))}")
         for name, value in params.items()
     ])
 

test/test_text.py
@@ -633,6 +633,10 @@ class TestText(unittest.TestCase):
         self.assertEqual(f({"foo": "1"}), "foo=1")
         self.assertEqual(f({"foo": "1", "bar": "2"}), "foo=1&bar=2")
 
+        # int values
+        self.assertEqual(f({"foo": 1}), "foo=1")
+        self.assertEqual(f({"foo": 1, "bar": "2"}), "foo=1&bar=2")
+
         # missing value
         self.assertEqual(f({"bar": ""}), "bar=")
         self.assertEqual(f({"foo": "1", "bar": ""}), "foo=1&bar=")

commit a153278f632ac0ab01fc3e9eece3d78e8586a024

[text] restore 'bytes' value usage in 'build_query()' (#106)
fixes regression introduced in 0f63a5930ead1a6b6c5d8ea28c386f924ae5713f

gallery_dl/text.py
@@ -379,7 +379,7 @@ def parse_query_list(qs, as_list=()):
 def build_query(params):
     return "&".join([
         (f"{quote(name)}="
-         f"{quote(value if isinstance(value, str) else str(value))}")
+         f"{quote(str(value) if isinstance(value, int) else value)}")
         for name, value in params.items()
     ])
 

test/test_text.py
@@ -633,6 +633,10 @@ class TestText(unittest.TestCase):
         self.assertEqual(f({"foo": "1"}), "foo=1")
         self.assertEqual(f({"foo": "1", "bar": "2"}), "foo=1&bar=2")
 
+        # bytes values
+        self.assertEqual(f({"foo": b"1"}), "foo=1")
+        self.assertEqual(f({"foo": b"1", "bar": "2"}), "foo=1&bar=2")
+
         # int values
         self.assertEqual(f({"foo": 1}), "foo=1")
         self.assertEqual(f({"foo": 1, "bar": "2"}), "foo=1&bar=2")