mikf/gallery-dl

commit 270aaea8ab6153407bca89b665757de3776a2a11

[pixiv] provide fallback URLs (#6762)

gallery_dl/extractor/pixiv.py
@@ -111,6 +111,7 @@ class PixivExtractor(Extractor):
                 {
                     "url"   : img["image_urls"]["original"],
                     "suffix": "_p{:02}".format(num),
+                    "_fallback": self._fallback_image(img),
                 }
                 for num, img in enumerate(meta_pages)
             ]
@@ -128,7 +129,7 @@ class PixivExtractor(Extractor):
             self.log.warning("%s: 'My pixiv' locked", work["id"])
 
         elif work["type"] != "ugoira":
-            return ({"url": url},)
+            return ({"url": url, "_fallback": self._fallback_image(url)},)
 
         elif self.load_ugoira:
             try:
@@ -268,6 +269,24 @@ class PixivExtractor(Extractor):
                 return url
             except exception.HttpError:
                 pass
+
+    def _fallback_image(self, src):
+        if isinstance(src, str):
+            urls = None
+            orig = src
+        else:
+            urls = src["image_urls"]
+            orig = urls["original"]
+
+        base = orig.rpartition(".")[0]
+        yield base.replace("-original/", "-master/", 1) + "_master1200.jpg"
+
+        if urls is None:
+            return
+
+        for fmt in ("large", "medium", "square_medium"):
+            if fmt in urls:
+                yield urls[fmt]
 
     @staticmethod
     def _date_from_url(url, offset=timedelta(hours=9)):

test/results/pixiv.py
@@ -290,6 +290,15 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://www.pixiv.net/artworks/56360615",
+    "#comment" : "fallback; 'original' version results in HTTP 500 error (#6762)",
+    "#class"   : pixiv.PixivWorkExtractor,
+    "#options" : {"retries": 0},
+    "#range"   : "4",
+    "#sha1_content": "aa119c27fec0a36bbd06e7491987acf5f1be6293",
+},
+
+{
     "#url"     : "https://www.pixiv.net/en/artworks/966412",
     "#category": ("", "pixiv", "work"),
     "#class"   : pixiv.PixivWorkExtractor,

RecRivenVI/Gallery-Downloader

commit 415ff4ad8ba362c4eda268fa22082b4a588d7034

[pixiv] restore 'background' fallback URLs (#9587)
fixes regression introduced in 270aaea8ab6153407bca89b665757de3776a2a11

gallery_dl/extractor/pixiv.py
@@ -173,7 +173,11 @@ class PixivExtractor(Extractor):
                                work_id, limit_type)
 
         elif work["type"] != "ugoira":
-            return ({"url": url, "_fallback": self._fallback_image(url)},)
+            return ({
+                "url": url,
+                "_fallback": (work.get("_fallback") or
+                              self._fallback_image(url)),
+            },)
 
         elif self.load_ugoira:
             try:

gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.32.3"
+__version__ = "1.32.4-dev"
 __variant__ = None

test/results/pixiv.py
@@ -217,6 +217,7 @@ __tests__ = (
         "captions"         : True,
         "comments"         : True,
     },
+    "#results"     : "https://i.pximg.net/user-profile/img/2008/06/17/01/28/01/171098_fc06efd15628e2ee252941ae5298b5ff.jpg",
     "#sha1_content": "4e57544480cc2036ea9608103e8f024fa737fe66",
 },
 
@@ -229,7 +230,12 @@ __tests__ = (
         "captions"         : True,
         "comments"         : True,
     },
-    "#pattern" : r"https://i\.pximg\.net/background/img/2021/01/30/16/12/02/194921_af1f71e557a42f499213d4b9eaccc0f8\.jpg",
+    "#results": "https://i.pximg.net/background/img/2021/01/30/16/12/02/194921_af1f71e557a42f499213d4b9eaccc0f8.jpg",
+
+    "_fallback": (
+        "https://i.pximg.net/background/img/2021/01/30/16/12/02/194921_af1f71e557a42f499213d4b9eaccc0f8.png",
+        "https://i.pximg.net/background/img/2021/01/30/16/12/02/194921_af1f71e557a42f499213d4b9eaccc0f8.gif",
+    ),
 },
 
 {