RecRivenVI/Gallery-Downloader

commit 186fdf703a3e8e73073ba05f936f1e579712ffb9

[aryion] extract 'title' without HTML entities (#183)

gallery_dl/extractor/aryion.py
@@ -195,7 +195,7 @@ class AryionExtractor(Extractor):
         extr = text.extract_from(self.request(post_url).text)
 
         title, _, artist = text.unescape(extr(
-            "<title>g4 :: ", "<")).rpartition(" by ")
+            'property="og:title" content="', '"')).rpartition(" by ")
 
         return {
             "id"    : text.parse_int(post_id),

test/results/aryion.py
@@ -128,6 +128,24 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://aryion.com/g4/view/1244524",
+    "#comment" : "'&amp;' in title (#183)",
+    "#class"   : aryion.AryionPostExtractor,
+    "#results" : "https://aryion.com/g4/data.php?id=1244524",
+
+    "artist"     : "Dotil",
+    "date"       : "dt:2026-03-17 05:15:48",
+    "description": "Patreon request",
+    "width"      : 4141,
+    "height"     : 2331,
+    "id"         : 1244524,
+    "path"       : ["Patreon stuff"],
+    "size"       : 2776447,
+    "title"      : "Vesta & Eliza",
+    "url"        : "https://aryion.com/g4/data.php?id=1244524",
+},
+
+{
     "#url"     : "https://aryion.com/g4/search.php?q=forest1",
     "#class"   : aryion.AryionSearchExtractor,
     "#results" : (

commit 973639416e4040c2a43b6ead6a4a37a476fb3a85

[aryion] fix metadata extraction (#193)
fixes regression introduced in 186fdf703a3e8e73073ba05f936f1e579712ffb9

gallery_dl/extractor/aryion.py
@@ -195,13 +195,13 @@ class AryionExtractor(Extractor):
         extr = text.extract_from(self.request(post_url).text)
 
         title, _, artist = text.unescape(extr(
-            'property="og:title" content="', '"')).rpartition(" by ")
+            "<title>g4 :: ", "<")).rpartition(" by ")
 
         return {
             "id"    : text.parse_int(post_id),
             "url"   : url,
             "user"  : self.user or artist,
-            "title" : title,
+            "title" : text.unescape(title),
             "artist": artist,
             "description": text.unescape(extr(
                 'property="og:description" content="', '"')),
@@ -212,8 +212,8 @@ class AryionExtractor(Extractor):
             "views" : text.parse_int(extr("Views</b>:", "<").replace(",", "")),
             "width" : text.parse_int(extr("Resolution</b>:", "x")),
             "height": text.parse_int(extr("", "<")),
-            "comments" : text.parse_int(extr("Comments</b>:", "<")),
             "favorites": text.parse_int(extr("Favorites</b>:", "<")),
+            "comments" : text.parse_int(extr("Comments</b>:", "<")),
             "tags"     : text.split_html(extr("class='taglist'>", "</span>")),
             "filename" : fname,
             "extension": ext,

test/results/aryion.py
@@ -146,6 +146,162 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://aryion.com/g4/view/1157992",
+    "#comment" : "video",
+    "#class"   : aryion.AryionPostExtractor,
+    "#results" : "https://aryion.com/g4/data.php?id=1157992",
+
+    "artist"     : "Neoneo",
+    "comments"   : range(10, 100),
+    "date"       : "dt:2025-07-20 00:43:05",
+    "description": str,
+    "extension"  : "mp4",
+    "favorites"  : range(10, 1_000),
+    "filename"   : "Neoneo-1157992-Scene113",
+    "height"     : 964,
+    "id"         : 1157992,
+    "path"       : ["World of Alivia"],
+    "size"       : 17310159,
+    "tags"       : list,
+    "title"      : "Tonalnan's Invitation to Paradise [Anim]",
+    "url"        : "https://aryion.com/g4/data.php?id=1157992",
+    "user"       : "Neoneo",
+    "views"      : int,
+    "width"      : 1354,
+},
+
+{
+    "#url"     : "https://aryion.com/g4/view/1253659",
+    "#comment" : "text",
+    "#class"   : aryion.AryionPostExtractor,
+    "#results" : "https://aryion.com/g4/data.php?id=1253659",
+
+    "artist"     : "IddlerItaler",
+    "comments"   : range(5, 999),
+    "date"       : "dt:2026-05-11 21:00:24",
+    "description": "",
+    "extension"  : "rtf",
+    "favorites"  : range(20, 999),
+    "filename"   : "IddlerItaler-1253659-Interlude - The Perfect Seban Date",
+    "height"     : 0,
+    "id"         : 1253659,
+    "size"       : 281848,
+    "title"      : "Interlude - The Perfect Seban Date",
+    "url"        : "https://aryion.com/g4/data.php?id=1253659",
+    "user"       : "IddlerItaler",
+    "views"      : range(5_000, 50_000),
+    "width"      : 0,
+    "path"       : [
+        "World of Alivia",
+        "Multi-chapter Stories",
+        "Azure Fang",
+    ],
+    "tags"       : [
+        "adventure",
+        "alcohol",
+        "Anal Sex",
+        "animal prey",
+        "armadillo prey",
+        "Ayame Itoh",
+        "Azzath",
+        "Belch",
+        "Betting",
+        "blue hair",
+        "Blue Insides",
+        "Burping",
+        "burping up prey",
+        "Catgirl",
+        "cocky prey",
+        "cumflation",
+        "date",
+        "demi/demi",
+        "demi/feral",
+        "Digestion",
+        "dragon girl pred",
+        "Elf prey",
+        "empress pred",
+        "Endosoma",
+        "escape attempt",
+        "F/?",
+        "F/F",
+        "F/Futa",
+        "f/futa/f",
+        "F/M",
+        "f/multiple",
+        "Face First",
+        "Fantasy",
+        "Fatal",
+        "Female Pred",
+        "Female Prey",
+        "feral prey",
+        "Fish girl prey",
+        "Food Coma",
+        "foxgirl pred",
+        "Freezing",
+        "Frozen prey",
+        "Futa pred",
+        "Futa Prey",
+        "Futa/F",
+        "Futanari",
+        "Genderfluid prey",
+        "Goddess Pred",
+        "goddess prey",
+        "harpy prey",
+        "Head First",
+        "hot spring",
+        "humanoid pred",
+        "humanoid prey",
+        "Ice",
+        "ice digestion",
+        "Implied Disposal",
+        "Implied reformation",
+        "implied unwilling",
+        "Ivaler The Exile",
+        "Kana Rumaishi",
+        "khirishan",
+        "Kitsune",
+        "kitsune predator",
+        "Larger pred",
+        "Lesbian",
+        "Licking",
+        "Male Prey",
+        "Milk",
+        "Multiple Preds",
+        "Multiple Preys",
+        "Neko",
+        "Non-fatal",
+        "observer",
+        "Oral Vore",
+        "Original Character",
+        "penetration",
+        "Pinky Pleandurlas",
+        "pleasured pred",
+        "Pleasured Prey",
+        "purple hair",
+        "Regurgitation",
+        "Released prey",
+        "romance",
+        "Same Size",
+        "Sanako Sakawako",
+        "Sex",
+        "Sexual content",
+        "slight size difference",
+        "smaller prey",
+        "statue transformation",
+        "Story",
+        "Struggling",
+        "Stuffing",
+        "Tail",
+        "tanuki prey",
+        "Voreception",
+        "waiter prey",
+        "Willing prey",
+        "Wings",
+        "World of Alivia",
+    ],
+},
+
+{
     "#url"     : "https://aryion.com/g4/search.php?q=forest1",
     "#class"   : aryion.AryionSearchExtractor,
     "#results" : (