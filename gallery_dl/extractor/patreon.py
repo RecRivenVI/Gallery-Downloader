1ba5e95abe216f8225c1dfc4898ae9c803bcbade
Mike Fährmann <mike_faehrmann@web.de>
2026-07-30 18:46:04 +0200

    [patreon] fix 'campaign_id' extraction from '/cw/' creator pages (gh#9681)

--- gallery_dl/extractor/patreon.py
+++ gallery_dl/extractor/patreon.py
@@ -483,6 +483,13 @@ class PatreonCreatorExtractor(PatreonExtractor):
         if cid := text.extr(
                 page, r'{\"value\":{\"campaign\":{\"data\":{\"id\":\"', '\\"'):
             return cid
+        if cid := text.extr(
+                page, r'\":{\"campaign\":{\"data\":{\"id\":\"', '\\"'):
+            return cid
+
+        # thumbnail
+        if cid := text.extr(page, "/patreon-media/p/campaign/", "/"):
+            return cid
 
         raise self.exc.AbortExtraction("Failed to extract campaign ID")
 
