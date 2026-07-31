9e9c8566b3de822251d2030e2b8f223f3535b41e
Mike Fährmann <mike_faehrmann@web.de>
2026-07-31 19:34:54 +0200

    [patreon] add another 'campaign_id' source

--- gallery_dl/extractor/patreon.py
+++ gallery_dl/extractor/patreon.py
@@ -488,6 +488,9 @@ class PatreonCreatorExtractor(PatreonExtractor):
                 (cid := text.extr(curl, "/campaign/", "/")):
             return cid
 
+        if cid := text.extr(page, r'\"id\":\"NavigationBar_', '\\"'):
+            return cid
+
         raise self.exc.AbortExtraction("Failed to extract campaign ID")
 
     def _get_filters(self, params):
