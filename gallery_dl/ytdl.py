f882bc954dbb569b7caf19b00ef335289f903c75
Mike Fährmann <mike_faehrmann@web.de>
2026-08-09 18:42:47 +0200

    ensure backwards compatibility; add test

--- gallery_dl/ytdl.py
+++ gallery_dl/ytdl.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2021-2025 Mike Fährmann
+# Copyright 2021-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -11,7 +11,6 @@
 import shlex
 import itertools
 from . import text, util, exception
-from yt_dlp.networking.impersonate import ImpersonateTarget
 
 
 def import_module(module_name):
@@ -45,9 +44,6 @@ def construct_YoutubeDL(module, obj, user_opts, system_opts=None):
         opts = parse_command_line(module, argv) if argv else user_opts
     except SystemExit:
         raise exception.AbortExtraction("Invalid command-line option")
-
-    if opts.get("impersonate") is not None:
-        opts["impersonate"] = ImpersonateTarget.from_str(opts.get("impersonate"))
 
     if opts.get("format") is None:
         opts["format"] = config("format")
@@ -109,6 +105,9 @@ def parse_command_line(module, argv):
                 key, _, value = h.partition(":")
                 std_headers[key] = value
 
+    if getattr(opts, "impersonate", None) is not None:
+        opts.impersonate = module.ImpersonateTarget.from_str(
+            opts.impersonate.lower())
     if opts.ratelimit is not None:
         opts.ratelimit = parse_bytes(opts.ratelimit)
     if getattr(opts, "throttledratelimit", None) is not None:
@@ -449,7 +448,7 @@ def parse_command_line(module, argv):
         "geo_bypass_ip_block": getattr(
             opts, "geo_bypass_ip_block", None),
         "compat_opts": compat_opts,
-        "impersonate": opts.impersonate
+        "impersonate": getattr(opts, "impersonate", None),
     }
 
 
