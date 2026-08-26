e47414cab4eda0e9dfec346ea6c3a491d939f69b
Mike Fährmann <mike_faehrmann@web.de>
2026-08-26 18:25:15 +0200

    [mangayi] simplify code

--- gallery_dl/extractor/mangayi.py
+++ gallery_dl/extractor/mangayi.py
@@ -20,94 +20,56 @@ class MangayiBase():
 
 class MangayiChapterExtractor(MangayiBase, ChapterExtractor):
     """Extractor for manga-chapters from mangayi.com"""
-    pattern = BASE_PATTERN + r"(\/read\/[^/?#]+(?:\/v0*(\d+))?\/chapter\/(\d+[^/?#]*)\/)"
+    pattern = BASE_PATTERN + r"(/read/[^/?#]+/(?:v(\d+)/)?chapter/\d+[^/?#]*/)"
     example = "https://mangayi.com/read/TITLE/chapter/1/"
 
-    def __init__(self, match):
-        self.part, self.volume, self.chapter = match.groups()
-        self.base = f"{self.root}{self.part}"
-        ChapterExtractor.__init__(self, match, self.base)
-
-    def get_pages_count(self, page) -> int:
-        """Return the count of manga pages"""
-        page_template='type="application/ld+json">{"@context":"https://schema.org","@type":"MediaGallery",'
-        
-        gallery, pos = text.extract(page, page_template, '</script>')
-        gallery = util.json_loads('{' + gallery)
-
-        return len(gallery['associatedMedia'])
-
-    def get_page_json(self, page) -> dict:
-        """Extract basic metadata from manga page"""
-        page_template='type="application/ld+json">{"@context":"https://schema.org","@type":"ComicIssue",'
-        
-        page, pos = text.extract(page, page_template, '</script>')
-        page = util.json_loads('{' + page)
-
-        return page
-
-
     def metadata(self, page):
-        data = self.get_page_json(page)
-        chapter, dot, minor = data['issueNumber'].partition('.')
-        
+        data = util.json_loads("{" + text.extr(
+            page, '","@type":"ComicIssue",', '</script>'))
+        chapter, dot, minor = data["issueNumber"].partition(".")
+
         return {
-            "manga"         :   data['isPartOf']['name'],
-            "title"         :   data['name'].split(":")[-1].lstrip(' '),
-            "chapter"       :   text.parse_int(chapter),
-            "chapter_minor" :   dot + minor,
-            "count"         :   self.get_pages_count(page),
-            "lang"          :   "en",
-            "language"      :   "English"
+            "manga"        : data["isPartOf"]["name"],
+            "title"        : data["name"].partition(":")[2].strip(),
+            "volume"       : text.parse_int(self.groups[1]),
+            "chapter"      : text.parse_int(chapter),
+            "chapter_minor": dot + minor,
+            "date"         : self.parse_datetime_iso(data["datePublished"]),
+            "lang"         : data["inLanguage"],
         }
 
-
     def images(self, page):
-        mediaGalleryTemplate = 'type="application/ld+json">{"@context":"https://schema.org","@type":"MediaGallery",'
+        data = util.json_loads("{" + text.extr(
+            page, '","@type":"MediaGallery",', '</script>'))
 
-        page, pos = text.extract(page, mediaGalleryTemplate, '</script>')
-        page = util.json_loads('{' + page)['associatedMedia']
-
-        for image in page:
-            yield text.ensure_http_scheme(image['url']), None
+        return [
+            (text.ensure_http_scheme(image["url"]), None)
+            for image in data["associatedMedia"]
+        ]
 
 
 class MangayiMangaExtractor(MangayiBase, MangaExtractor):
     chapterclass = MangayiChapterExtractor
-    pattern = BASE_PATTERN + r"(\/read\/[^/?#]+\/)"
+    pattern = BASE_PATTERN + r"(/read/[^/?#]+/)"
     example = "https://mangayi.com/read/TITLE"
 
     def chapters(self, page):
-        chaptersTemplate='type="application/ld+json">{"@context":"https://schema.org","@type":"ItemList",'
+        manga = util.json_loads("{" + text.extr(
+            page, '","@type":"ComicSeries",', '</script>'))["name"]
+        data = util.json_loads("{" + text.extr(
+            page, '","@type":"ItemList",', '</script>'))
+
         results = []
+        for ch in data["itemListElement"]:
+            url = ch["url"]
+            chapter = url[url.rfind("/", 0, -2)+1:-1]
+            chapter, sep, minor = chapter.partition("-")
 
-        manga, pos = text.extract(page, '<title>', '</title>')
-        manga = manga.removesuffix(' Manga - Read Free Online | Mangayi')
-        manga = text.unescape(manga)
-
-        page, pos = text.extract(page, chaptersTemplate, '</script>')
-        page = util.json_loads('{' + page)
-
-        chapters_list = page['itemListElement']
-
-        for ch in chapters_list:
-            chapter = ch['url'].split('/')[-2]
-            title = ch['name'].removeprefix(manga).lstrip()
-
-            chapter, sep, minor = chapter.partition('-')
-            
-            results.append(
-                (
-                    ch['url'], 
-                    {
-                        "manga": manga,
-                        "chapter": text.parse_int(chapter),
-                        "chapter_minor": sep + minor,
-                        "title": title,
-                        "date": None,
-                        "lang": "en",
-                        "language": "English"
-                    }
-                )
-            )
+            results.append((url, {
+                "manga"        : manga,
+                "chapter"      : text.parse_int(chapter),
+                "chapter_minor": sep + minor,
+                "title"        : ch["name"].partition(":")[2].strip(),
+                "lang"         : "en",
+            }))
         return results
