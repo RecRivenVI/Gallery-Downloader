RecRivenVI/Gallery-Downloader

commit ef3df4137d10644c9a0cc003339f2482d019356b

[gofile] fix single file links (#375)

gallery_dl/extractor/gofile.py
@@ -33,13 +33,14 @@ class GofileFolderExtractor(Extractor):
         folder = self._get_content(self.groups[0], password)
         yield Message.Directory, "", folder
 
-        try:
-            contents = folder.pop("children")
-        except KeyError:
+        if not folder.get("canAccess"):
             raise self.exc.AuthorizationError("Password required")
 
+        contents = (folder.pop("children").values()
+                    if "children" in folder else (folder,))
+
         num = 0
-        for content in contents.values():
+        for content in contents:
             content["folder"] = folder
 
             if content["type"] == "file":

commit 72bf80481ccd8eff8130cdeb91b7f38eb8a5099b

[gofile] fix 'RecursionError' in single file links (#430)
fixes regression introduced in ef3df4137d10644c9a0cc003339f2482d019356b

gallery_dl/extractor/gofile.py
@@ -36,8 +36,11 @@ class GofileFolderExtractor(Extractor):
         if not folder.get("canAccess"):
             raise self.exc.AuthorizationError("Password required")
 
-        contents = (folder.pop("children").values()
-                    if "children" in folder else (folder,))
+        if "children" in folder:
+            contents = folder.pop("children").values()
+        else:
+            contents = (folder,)
+            folder = None
 
         num = 0
         for content in contents: